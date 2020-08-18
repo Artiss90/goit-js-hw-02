5510f5f628664d75ed04e49058c203fa286a05a5
Art <Petrenkoartik@gmail.com>
2020-08-18 23:31:05 +0300

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,24 @@
 'use strict';
+const getItemsString = function (array) {
+  for (let i = 0; i < array.length; i += 1)
+    console.log(`${i + 1} - ${array[i]}`);
+};
+
+console.log(getItemsString(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']));
+/*
+'1 - Mango
+2 - Poly
+3 - Ajax
+4 - Lux
+5 - Jay
+6 - Kong
+'
+*/
+
+console.log(getItemsString([5, 10, 15]));
+/*
+'1 - 5
+2 - 10
+3 - 15
+'
+*/
