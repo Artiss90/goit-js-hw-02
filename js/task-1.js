52795305aafb1ba96ddafa21bdf71659687a8432
Art <Petrenkoartik@gmail.com>
2020-08-19 17:13:55 +0300

    task-1 performed

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,11 @@
 'use strict';
 const getItemsString = function (array) {
-  for (let i = 0; i < array.length; i += 1)
-    console.log(`${i + 1} - ${array[i]}`);
+  let result = '';
+  for (let i = 0; i < array.length; i += 1) {
+    result += `${i + 1} - ${array[i]}\n`;
+  }
+  // console.log(result);
+  return result;
 };
 
 console.log(getItemsString(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']));
