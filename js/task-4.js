a482a09894dc85af6dc0abba6e949bb48e6727f4
Art <Petrenkoartik@gmail.com>
2020-08-22 19:46:45 +0300

    task-4 complete

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,24 @@
 'use strict';
+function formatString(string = '', maxLength = 40) {
+  let result;
+  let lengthString = string.split('');
+  console.log(lengthString.length);
+  if (lengthString.length > maxLength) {
+    lengthString.length = maxLength;
+    return (result = lengthString.join('') + '...');
+  } else {
+    result = string;
+  }
+  return result;
+}
+
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// 'Curabitur ligula sapien, tincidunt non.'
+
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// 'Vestibulum facilisis, purus nec pulvinar...'
+
+console.log(
+  formatString('Vestibulum facilisis, purus nec pulvinar iaculis.', 30),
+);
+// 'Vestibulum facilisis, purus ne...'
