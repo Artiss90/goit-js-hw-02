ba707cc9908ae7a2bc3ff4769bc4f10ed5e283fd
Art <Petrenkoartik@gmail.com>
2020-08-22 20:16:35 +0300

    task-5 complete

--- js/task-5.js
+++ js/task-5.js
@@ -1,1 +1,13 @@
 'use strict';
+function checkForSpam(message) {
+  let lowerString = message.toLowerCase();
+  return lowerString.includes('spam') || lowerString.includes('sale');
+}
+
+console.log(checkForSpam('Latest technology news')); // false
+
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+console.log(checkForSpam('Get best sale offers now!')); // true
+
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
